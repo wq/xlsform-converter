398bfe2ca8ccdc3d77bbb9f7e1b62b7b0045f630
S. Andrew Sheppard <andrew@wq.io>
2016-01-27 01:12:36 -0500

    more api available from top level module

--- xlsconv/__init__.py
+++ xlsconv/__init__.py
@@ -1,5 +1,14 @@
-from .html import xls2html
-from .django import xls2django
+from .html import xls2html, html_context
+from .django import xls2django, django_context
+from .parser import parse_xls
+from .renderer import render
 
 
-__all__ = ['xls2html', 'xls2django']
+__all__ = [
+    'xls2html',
+    'html_context',
+    'xls2django',
+    'django_context',
+    'parse_xls',
+    'render',
+]
