b364c448fc393495898e4675434eb4ee750f6811
S. Andrew Sheppard <andrew@wq.io>
2017-04-04 12:47:18 -0500

    add python 3.6

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Text Processing :: Markup :: HTML',
     ],
     test_suite='tests',
